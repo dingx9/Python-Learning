b99603e6798c8a2d3cb8f41e7f4e2e12133e10c8
dingx9 <54413639+dingx9@users.noreply.github.com>
2025-09-25 03:19:48 +0800

    Update ultimate_password_game.py

--- projects/ultimate_password_game.py
+++ projects/ultimate_password_game.py
@@ -43,3 +43,6 @@ def ultimate_password_game(min_num=1, max_num=100):
 if __name__ == "__main__":
     ultimate_password_game(1,50)  ##範圍設定(1~50)
 
+#封裝成 ultimate_password_game() 函式，可指定範圍。
+#輸入檢查，避免非整數輸入錯誤。
+#提示範圍會隨猜測自動更新。
